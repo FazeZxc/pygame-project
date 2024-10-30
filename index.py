f8a55e3e6b897a3ff38a4a7720fa24b3b37a9da3
Deependra0043 <sgauraw52@gmail.com>
2024-10-30 09:52:15 +0530

    3

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ import pygame
 import math
 pygame.init()
 
-winWidth = 840;
-winHeight = 480;               
+winWidth = 840
+winHeight = 480           
 
 win = pygame.display.set_mode((winWidth, winHeight))
 pygame.display.set_caption("game")
